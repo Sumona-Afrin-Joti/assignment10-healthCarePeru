86d4ec9988100ddd3672e56962c780cc648e4a7f
Sumona-Afrin-Joti <sumonajoti.06@gmail.com>
2021-11-28 10:26:48 +0600

    updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDmOr5RH6WLKQfWVh_zlunCqIT5zr-IxVc",
-  authDomain: "assignment-10-10c03.firebaseapp.com",
-  projectId: "assignment-10-10c03",
-  storageBucket: "assignment-10-10c03.appspot.com",
-  messagingSenderId: "69676931500",
-  appId: "1:69676931500:web:79d2ac46eeb5f5c48dc209"
+  apiKey: "AIzaSyDANjq_XRQO_XioWiTVHFA-w8CDLXirHhs",
+  authDomain: "perhu-fbf72.firebaseapp.com",
+  projectId: "perhu-fbf72",
+  storageBucket: "perhu-fbf72.appspot.com",
+  messagingSenderId: "1052725763194",
+  appId: "1:1052725763194:web:07cb191e1433160920afc3"
 };
 
 export default firebaseConfig;
